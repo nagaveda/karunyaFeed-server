3a702fdee665fd9e6d454fcc563d6a97b3701050
nagaveda <gajjalanagaveda@karunya.edu.in>
2020-06-25 19:35:32 +0530

    added functionality for posting , server handling and storing images in cloudonary

--- routes/post.js
+++ routes/post.js
@@ -17,14 +17,15 @@ router.get('/allposts',(req, res)=> {
 
 
 router.post('/createpost', requireLogin , (req, res) => {
-    const {title, body} = req.body;
-    if(!title || !body){
+    const {title, body, pic} = req.body;
+    if(!title || !body || !pic){
         return res.status(422).json({error: "Please add all the fields."})
     }
     req.user.password = undefined;
     const post = new Post({
         title,
         body,
+        photo: pic,
         postedBy: req.user
          
     });
